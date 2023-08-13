1cd5f20deea55ae21f7a8f8c6b1da6dc8b930b36
Cameron Smith <cameron.smith@kyndryl.com>
2023-08-14 09:51:14 +1000

    New version

--- CreateCSR.ps1
+++ CreateCSR.ps1
@@ -3,25 +3,12 @@
 #
 # https://www.sslplus.de/wiki-en/Generating_a_CSR_in_MS_Windows_(using_certreq)
 # -------------------------------------------------------------------------------
-param (
-      #----- Define parameters -----#
-      [String]$jumpBox = ''                                                     # Certiificate Authority server location  
-)
+
     Write-host "Running Create"
     #Get the local values
     $fqdn = [System.Net.Dns]::GetHostByName($env:computerName).HostName
     $IPaddr = (Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null -and $_.NetAdapter.status -ne "Disconnected"}).IPv4Address.IPAddress
     
-    $CSRFolder = "\\{0}\C$\Kyndryl\CSR" -f $jumpBox
-    If (-not (Test-Path $CSRFolder)) {
-        # Folder does not exist, create it
-        Try{
-            New-Item -Path $CSRFolder -ItemType Directory
-            Write-host "Folder created"
-        } Catch{
-            Exit 2
-        }
-    }
     
     $INFFolder = "C:\Kyndryl"
     If (-not (Test-Path $INFFolder)) {
@@ -34,6 +21,17 @@ param (
         }    
     }
 
+    $CSRFolder = "C:\Kyndryl\CSR"  
+    If (-not (Test-Path $CSRFolder)) {
+        # Folder does not exist, create it
+        Try{
+            New-Item -Path $CSRFolder -ItemType Directory
+            Write-host "Folder created"
+        } Catch{
+            Exit 2
+        }
+    }
+      
     $CSRPath = "{0}\{1}.csr" -f $CSRFolder,$fqdn 
     $INFPath = "{0}\{1}.inf" -f $INFFolder,$fqdn
 
